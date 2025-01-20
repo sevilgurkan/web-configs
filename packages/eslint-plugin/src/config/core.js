cbeba4b3d2beefca99e4e2da9e05df104dceef76
Gürkan Sevil <sevilgurkann@gmail.com>
2025-01-20 14:16:12 +0300

    chore(eslint): update core config

--- packages/eslint-plugin/src/config/core.js
+++ packages/eslint-plugin/src/config/core.js
@@ -1,14 +1,30 @@
 const eslintJs = require('@eslint/js');
 const importPlugin = require('eslint-plugin-import');
 
+const fmssPlugin = require('../plugin');
+const {OFF, ERROR} = require('../utilities');
+
 /** @type {import('eslint').Linter.Config[]} */
 module.exports = [
-  eslintJs.configs.recommended,
-  importPlugin.flatConfigs.recommended,
+  // eslintJs.configs.recommended,
+  // importPlugin.flatConfigs.recommended,
   {
+    plugins: {
+      '@fmss': fmssPlugin,
+    },
     rules: {
       ...require('../rules/best-practice').rules,
       ...require('../rules/import').rules,
+    },
+  },
+  {
+    rules: {
+      // FMSS
+
+      // Require (or disallow) assignments of binary, boolean-producing expressions to be wrapped in parentheses.
+      '@fmss/binary-assignment-parens': [ERROR, 'always'],
+      // Prevent namespace import declarations
+      '@fmss/no-namespace-imports': ERROR,
     },
   },
 ];
